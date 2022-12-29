90d135777dab8c9cff0b019f0d772e6992f2d5fd
Temo <tlomsadze3@gmail.com>
2022-12-30 01:55:32 +0400

    first commit

---
+++ Documents/machine-learning/SwrfiviRegresia.h
@@ -0,0 +1,28 @@
+#ifndef ML_LINEARREGRESSION_H
+#define ML_LINEARREGRESSION_H
+
+class LinearRegression {
+
+public:
+    double *x;
+    double *y;
+    int m;
+    double *theta;
+    LinearRegression(double x[], double y[], int m);
+    /**
+     * @param alpha
+     * @param iterations
+     */
+
+    void train(double alpha, int iterations);
+    double predict(double x);
+
+private:
+    static double compute_cost(double x[], double y[], double theta[], int m);
+    static double h(double x, double theta[]);
+    static double *calculate_predictions(double x[], double theta[], int m);
+    static double *gradient_descent(double x[], double y[], double alpha, int iters, double *J, int m);
+};
+
+
+#endif
